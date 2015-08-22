978e2ac85cf713baf1d83752771d56f8504fa4d1
guilhermitoo <gh.dias1995@gmail.com>
2015-08-22 13:47:50 -0300

    adicionando projetos testes de php

---
+++ teste1/index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html> 
+<html> 
+	<body> 
+		<h1>testando PHP!</h1> 
+		<?php 		
+			echo '<input class="text" id="nome">'		
+		?>
+		<button type="button" class="btn" id="botao">Teste</button>
+		
+		
+	</body> 
+</html> 
+
---
+++ teste2/script.php
@@ -0,0 +1,7 @@
+<?php
+	// formulário php que ira trabalhar com o post
+	echo "o valor do campo 1: " . $_POST["campo1"];
+	echo "<br>o valor do campo 2: " . $_POST["campo2"];
+
+	echo "<br><br>O S.O selecionado: " . $_POST["sistema"];
+?>
