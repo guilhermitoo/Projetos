754eab246421d098615c67ce67b3d72f76cb8da7
guilhermitoo <gh.dias1995@gmail.com>
2015-09-08 18:40:52 -0300

    alterações no pedidos - pdv

--- pdv/pedidos.php
+++ pdv/pedidos.php
@@ -1,1 +1,23 @@
 <?
+    require_once 'medoo.min.php';
+ 
+    $database = new medoo();		
+
+		$database->pdo->beginTransaction();
+		try
+		{
+			// insere o pedido
+			$id = $database->insert("pedidos",[
+					"cliente_id"=> (integer) $_POST["cliente_id"],
+					"valor_total"=> (float) $_POST["valor_total"]
+			]);
+			//insere os itens	
+			
+			//se chegar aqui, faz o commit
+			$database->pdo->commit();
+		}
+		catch()
+		{
+			$database->pdo->rollback();
+		}
+		
