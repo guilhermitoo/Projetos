5f7e7b0e6d09e8f92fcfb6c6f87512ae52213d83
Guilherme Henrique da Cruz Dias <gh.dias1995@gmail.com>
2015-09-09 10:44:25 -0300

    Update pedidos.php

--- pdv/pedidos.php
+++ pdv/pedidos.php
@@ -6,18 +6,31 @@
 		$database->pdo->beginTransaction();
 		try
 		{
+			$cliente_id = $_POST["cliente_id"];
+			$valor_total = $_POST["valor_total"];
+			$produtos = $json_decode($json_encode($_POST["produtos"]));
+			
 			// insere o pedido
 			$id = $database->insert("pedidos",[
-					"cliente_id"=> (integer) $_POST["cliente_id"],
-					"valor_total"=> (float) $_POST["valor_total"]
+					"cliente_id"=> (integer) $cliente_id,
+					"valor_total"=> (float) $valor_total
 			]);
 			//insere os itens	
-			
+			for ( $i = 0 ; $i < $produtos.length; $i++ ) {
+				$id = $database->insert("pedidos_itens",[
+					"pedido_id"=> (integer) $id,
+					"produto_id"=> (integer) $produtos[$i].id,
+					"quantidade"=> (integer) $produtos[$i].quantidade,
+					"preco"=> (float) $produtos[$i].valor,
+				]);
+			}
 			//se chegar aqui, faz o commit
 			$database->pdo->commit();
+			echo 'Pedido cadastrado com sucesso!';
 		}
-		catch()
+		catch(PDOException $e)
 		{
 			$database->pdo->rollback();
+			echo $e->getMessage();
 		}
 		
