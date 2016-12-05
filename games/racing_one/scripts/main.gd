68be67f69c54516e31e18c10a4ecb66b0b32adce
Guilherme Henrique da Cruz Dias <gh.dias1995@gmail.com>
2016-12-05 13:51:43 -0200

    Novo projeto, RACING_ONE

---
+++ games/racing_one/scripts/main.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	
+	OS.set_window_position(Vector2(0,0))
+	
+	pass
