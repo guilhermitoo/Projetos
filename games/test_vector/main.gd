522ae5273aaef6a07ef0072754818b042efeacdb
Guilherme Henrique da Cruz Dias <gh.dias1995@gmail.com>
2017-05-17 13:37:28 -0300

    Tiros em direção a posição do mouse.

--- games/test_vector/main.gd
+++ games/test_vector/main.gd
@@ -6,6 +6,7 @@ extends Node
 # var b = "textvar"
 var pos = Vector2( )
 var pre_tiro = preload("res://tiro.tscn");
+var delay = 0;
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -15,8 +16,22 @@ func _ready():
 	pass	
 
 func _process(delta):
+	
 	if Input.is_action_pressed("shoot"):
-		var scalar = get_node("Area2D").get_global_pos().dot(game.getCamera().get_global_mouse_pos())
-		var tiro = pre_tiro.instance(scalar)
-		tiro.set_pos(get_node("Area2D").get_global_pos())
-		game.getMain().add_child(tiro)
+		delay -= delta;
+		if delay <= 0:
+			var origin = Vector2(1,0)
+			var a = get_node("Area2D").get_global_pos()
+			# b = mouse
+			var b = game.getCamera().get_global_mouse_pos()
+
+			var tiro = pre_tiro.instance()
+
+			var ang = a.angle_to_point(b)
+			tiro.set_pos(get_node("Area2D").get_global_pos())
+			tiro.rotate(ang)
+			#tiro.look_at(b)
+			game.getMain().add_child(tiro)
+			tiro.mousepos = get_node("Area2D").get_local_mouse_pos()
+			delay = 0.1;
+		
--- games/test_vector/tiro.gd
+++ games/test_vector/tiro.gd
@@ -3,12 +3,16 @@ extends Area2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var mousepos = Vector2(0,0)
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	set_process(true)
+	#mousepos = game.getCamera().get_global_mouse_pos()
 	pass
 
-func _init(rotation):
-	rotate(rotation);
+func _process(delta):
+	if mousepos != Vector2(0,0):
+		var a = mousepos*delta
+		translate(a)
