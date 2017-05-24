bae113a5ca680ea2f66e5fe0ced6335922850890
Guilherme Henrique da Cruz Dias <gh.dias1995@gmail.com>
2017-05-24 13:14:20 -0300

    ajustes inimigo, criada class_enemy

--- games/spaceship_one/scripts/meteor.gd
+++ games/spaceship_one/scripts/meteor.gd
@@ -1,31 +1,12 @@
-extends Area2D
+extends "res://scripts/class_enemy.gd"
 
 var vel
 var rotation
 var scale
-export var vida = 0
 
 var pre_pu_duplo = preload("res://scripts/classes/bonus/class_pu_tiro_duplo.gd")
 var pre_pu_rapido = preload("res://scripts/classes/bonus/class_pu_tiro_rapido.gd")
 var pre_pu_basic_shield = preload("res://scripts/classes/bonus/class_pu_basic_shield.gd")
-
-func _ready():
-	# adiciona o meteoro criado ao grupo inimigos
-	add_to_group(game.GRUPO_INIMIGO)
-	# melhora o randomico, cria uma nova semente para tomar como
-	# base no rand_rage
-	randomize()
-	set_process(true)
-	# GERA UM NÚMERO ALEATÓRIO ENTRE -20 E 20 
-	# PARA UTILIZAR NA FUNÇÃO DE ROTAÇÃO (ESQUERDA OU DIREITA ALEATORIO)
-	
-	rotation = rand_range(game.meteor_rot_ini,game.meteor_rot_fim)
-	vel = rand_range(game.meteor_vel_ini,game.meteor_vel_fim)
-	scale = rand_range(game.meteor_scale_ini,game.meteor_scale_fim)
-	vida = game.meteor_hp
-	
-	set_scale(Vector2(scale,scale))
-	pass
 
 func _process(delta):
 	# DESCE
@@ -34,31 +15,8 @@ func _process(delta):
 	rotate(deg2rad(rotation*delta))
 	
 	if game.estaForaTela(self):
-		print("meteor_free")
+		print("enemy_free")
 		queue_free()
-	
-	pass
-	
-func aplica_dano(valor,nave):
-	vida -= valor
-	get_node("anim").play("hit")
-	if vida <= 0:
-		# acresce PONTUACAO DO JOGO
-		if nave.is_in_group(game.GRUPO_NAVE):
-			game.score += game.pontuacao_por_meteoro
-		set_z(10)
-		# remove do grupo INIMIGO
-		remove_from_group(game.GRUPO_INIMIGO)
-		# executa a animação de destruição
-		get_node("anim").play("destroy")
-		# para o processamento
-		set_process(false)
-		# executa a animação de balançar a câmera
-		game.getCamera().shake()
-		# GERA UM POWER UP AO DESTRUIR O METEORO
-		#gerar_pu()
-		# executa o som de destruição
-		get_node("sample").play("explosion")
 	pass
 	
 func gerar_pu():
@@ -70,3 +28,23 @@ func gerar_pu():
 		basic_shield.resultar_powerup(vel*0.5)
 	
 	pass
+
+func _destroi():
+	# executa a animação de destruição
+	get_node("anim").play("destroy")
+	# GERA UM POWER UP AO DESTRUIR O METEORO
+	#gerar_pu()
+	# executa o som de destruição
+	get_node("sample").play("explosion")
+	pass
+	
+func _inicia():
+	# melhora o randomico, cria uma nova semente para tomar como
+	# base no rand_rage
+	randomize()
+	rotation = rand_range(game.meteor_rot_ini,game.meteor_rot_fim)
+	vel = rand_range(game.meteor_vel_ini,game.meteor_vel_fim)
+	scale = rand_range(game.meteor_scale_ini,game.meteor_scale_fim)
+	vida = game.meteor_hp
+	
+	set_scale(Vector2(scale,scale))
