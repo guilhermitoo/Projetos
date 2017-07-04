e8c3a118c2618d36122b7018e0b43b25c77bcea1
Guilherme Henrique da Cruz Dias <gh.dias1995@gmail.com>
2017-07-04 15:06:37 -0300

    testes perseguir o mouse e olhar para o mouse.

--- games/test_vector/main.gd
+++ games/test_vector/main.gd
@@ -16,22 +16,21 @@ func _ready():
 	pass	
 
 func _process(delta):
+	get_node("Area2D/Sprite").look_at(get_node("Area2D").get_global_mouse_pos())
+	var character = get_node("Area2D")
+	var mouseglobal = get_node("Area2D").get_global_mouse_pos()
+	var mouselocal = get_node("Area2D").get_local_mouse_pos()
+	
+	if Input.is_action_pressed("ui_up"):
+		character.translate(mouselocal.normalized()*delta*550)
 	
 	if Input.is_action_pressed("shoot"):
 		delay -= delta;
 		if delay <= 0:
-			var origin = Vector2(1,0)
-			var a = get_node("Area2D").get_global_pos()
-			# b = mouse
-			var b = get_node("Area2D").get_global_mouse_pos()
-
 			var tiro = pre_tiro.instance()
-
-			var ang = a.angle_to_point(b)
-			tiro.set_pos(get_node("Area2D").get_global_pos())
-			
-			tiro.look_at(b)
+			tiro.set_pos(character.get_global_pos())
+			tiro.look_at(mouseglobal)
 			game.getMain().add_child(tiro)
-			tiro.mousepos = get_node("Area2D").get_local_mouse_pos()
+			tiro.mousepos = mouselocal
 			delay = 0.1;
 		
