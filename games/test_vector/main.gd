226d9a82c7774f3c9f04661619102041be09ef0e
Guilherme Henrique da Cruz Dias <gh.dias1995@gmail.com>
2017-07-05 09:35:21 -0300

    ajustes movimentação do personagem.

--- games/test_vector/main.gd
+++ games/test_vector/main.gd
@@ -7,11 +7,11 @@ extends Node
 var pos = Vector2( )
 var pre_tiro = preload("res://tiro.tscn");
 var delay = 0;
+var speed = 1000
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	
 	set_process(true)
 	pass	
 
@@ -21,8 +21,15 @@ func _process(delta):
 	var mouseglobal = get_node("Area2D").get_global_mouse_pos()
 	var mouselocal = get_node("Area2D").get_local_mouse_pos()
 	
+	# MOVIMENTAÇÃO
 	if Input.is_action_pressed("ui_up"):
-		character.translate(mouselocal.normalized()*delta*550)
+		character.translate(Vector2(0,-1)*delta*speed)
+	if Input.is_action_pressed("ui_down"):
+		character.translate(Vector2(0,1)*delta*speed)
+	if Input.is_action_pressed("ui_left"):
+		character.translate(Vector2(-1,0)*delta*speed)
+	if Input.is_action_pressed("ui_right"):
+		character.translate(Vector2(1,0)*delta*speed)
 	
 	if Input.is_action_pressed("shoot"):
 		delay -= delta;
